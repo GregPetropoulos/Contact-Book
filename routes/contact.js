56146391373583ae9af95ad7969db876f913e129
GP <gregpetropoulos@yahoo.com>
2021-11-15 16:38:37 -0500

    adding routes folder

---
+++ routes/contact.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const router = express.Router();
+
+//route    GET api/contacts
+//@desc     Get all users contacts
+//@access   Private
+router.get('/', (req, res) => {
+  res.send('Get all users contacts');
+});
+
+//@route    POST api/contacts
+//@desc     Add new contacts
+//@access   Private
+router.post('/', (req, res) => {
+  res.send('Add users contacts');
+});
+
+//@route    PUT api/contacts/:id
+//@desc     Update a contact
+//@access   Private
+router.put('/:id', (req, res) => {
+  res.send('Update contact');
+});
+
+//@route    DELETE api/contacts/:id
+//@desc     Delete a contact
+//@access   Private
+router.delete('/:id', (req, res) => {
+  res.send('Delete a contact');
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,8 +1,21 @@
 const express = require('express');
-
 const app = express();
+// const responseTime = require('./middleware/responseTime');
 
+
+// MIDDLEWARE
+// app.use(responseTime);
+
+
+
+// ROUTE CHECK
 app.get('/', (req, res) => res.json({msg: "Welcome to the Contact Book Manager API"}));
+
+// DEFINED ROUTES, CALLS ROUTE FOLDER
+app.use('/api/users', require('./routes/users'));
+app.use('/api/auth', require('./routes/auth'));
+app.use('/api/contacts', require('./routes/contacts'));
+
 
 const PORT =process.env.PORT || 5000;
 
