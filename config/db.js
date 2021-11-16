ca36ea427bc62ad2f3e58680c77cf02d562e36ce
GP <gregpetropoulos@yahoo.com>
2021-11-16 07:39:12 -0500

    Connected to MongoDB used dotenv package to hide creds, server calls the db.

---
+++ config/db.js
@@ -0,0 +1,32 @@
+
+//BRING MONGOOSE --SIDE NOTE MONGOOSE RETURNS PROMISES
+const mongoose = require('mongoose');
+
+// PROTECT CREDS
+require('dotenv').config()
+
+//MONGOOSE TALK TO MONGODB UTILIZING THE CONFIG FILE (GLOBAL VARIABLES)
+// ***THESE CREDS WERE UNPROTECTED***//
+// const config = require('config');
+// const db = config.get('mongoURI');
+
+// PROTECT CREDS WITH THIS .ENV INSTEAD OF BRADS' DEFAULTJSON
+ const db = process.env.MY_MONGO_URI;
+
+
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(db);
+    console.log('MongoDB Connected....');
+  } catch (err) {
+    console.error(err.message);
+    process.exit(1);
+  }
+};
+module.exports = connectDB;
+
+//DEPRECATED MONGOOSE NOW AUTOMATICALLY RUNS THIS:
+// useNewUrlParser: true,
+// useCreateIndex: true,
+//useFindAndModify: false
--- server.js
+++ server.js
@@ -1,22 +1,28 @@
+//BRING IN EXPRESS
 const express = require('express');
-const app = express();
-// const responseTime = require('./middleware/responseTime');
 
+//BRING IN DB
+const connectDB = require('./config/db');
+
+const app = express();
+
+// INITIALIZE AND CONNECT TO DB
+connectDB();
+// const responseTime = require('./middleware/responseTime');
 
 // MIDDLEWARE
 // app.use(responseTime);
 
-
-
-// ROUTE CHECK
-app.get('/', (req, res) => res.json({msg: "Welcome to the Contact Book Manager API"}));
+// ROUTE TEST CHECK
+app.get('/', (req, res) =>
+  res.json({ msg: 'Welcome to the Contact Book Manager API' })
+);
 
 // DEFINED ROUTES, CALLS ROUTE FOLDER
 app.use('/api/users', require('./routes/users'));
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/contacts', require('./routes/contacts'));
 
-
-const PORT =process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
