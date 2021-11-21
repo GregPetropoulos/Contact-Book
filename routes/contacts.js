520a2b8ea9f7b2b632d9b7fbf198f4b3ec103ac3
GP <gregpetropoulos@yahoo.com>
2021-11-21 15:30:30 -0500

    A user has a add the contact route specific to that user. See the protected post route in routes/contact and verified this in postman with two different users registered and each user added  unique contacts.

--- routes/contacts.js
+++ routes/contacts.js
@@ -7,28 +7,50 @@ const auth = require('../middleware/auth');
 const User = require('../models/User');
 const Contact = require('../models/Contact');
 
-
 //* route    GET api/contacts
 //* @desc     Get all users contacts
 //* @access   Private
 router.get('/', auth, async (req, res) => {
-try {
-// * Look into Contact db and locate the user field with object id that matches from the auth middleware request id 
-  const contacts = await Contact.find({user: req.user.id}).sort({ date: -1})
-res.json(contacts)
-
-} catch (err) {
-  console.error(err.message);
-  req.statusCode(500).send('Server Error');
-}
+  try {
+    // * Look into Contact db and locate the user field with object id that matches from the auth middleware request id
+    const contacts = await Contact.find({ user: req.user.id }).sort({
+      date: -1
+    });
+    res.json(contacts);
+  } catch (err) {
+    console.error(err.message);
+    req.statusCode(500).send('Server Error');
+  }
 });
 
 //* @route    POST api/contacts
 //* @desc     Add new contacts
 //* @access   Private
-router.post('/', (req, res) => {
-  res.send('Add users contacts');
-});
+router.post(
+  '/',
+  [auth, [check('name', 'Name is required').not().isEmpty()]],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    const { name, email, phone, type } = req.body;
+    try {
+      const newContact = new Contact({
+        name,
+        email,
+        phone,
+        type,
+        user: req.user.id
+      });
+      const contact = await newContact.save();
+      res.json(contact);
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Server Error');
+    }
+  }
+);
 
 //* @route    PUT api/contacts/:id
 //* @desc     Update a contact
