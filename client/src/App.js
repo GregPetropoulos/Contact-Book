0642099c0c27959c808efe81a0b61e50d6bb4c9e
GP <gregpetropoulos@yahoo.com>
2021-11-22 08:02:52 -0500

    Removed from git from the client side, set up scripts in package.json server side and added proxy to package.json on client side, removed .gitignore from client side, cleaned up react app, created a navbar and home page, added app.css, all routes on front end work. Switch statement is now Routes and has updated with react-router-dom v6.

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,26 @@
-import React from 'react'
+import React, { Fragment } from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Navbar from './components/layout/Navbar';
+import Home from './components/pages/Home';
+import About from './components/pages/About';
+
 import './App.css';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <h1>contact book</h1>
-    </div>
+    <Router>
+      <Fragment>
+        <Navbar />
+<div className= 'container'>
+  <Routes>
+    <Route path = '/' element={<Home/>}/>
+    <Route path = '/about' element={<About/>}/>
+
+  </Routes>
+</div>
+      </Fragment>
+    </Router>
   );
-}
+};
 
 export default App;
