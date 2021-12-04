89b4c0b031047aa95ca064a9db03d1e4d61b91fb
GP <gregpetropoulos@yahoo.com>
2021-12-04 11:21:25 -0500

    preparing for deployment through heroku CLI.

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 //*BRING IN EXPRESS
 const express = require('express');
 
-
+//* File for path during build 
+const path = require ('path')
 
 //*BRING IN DB
 const connectDB = require('./config/db');
@@ -11,21 +12,12 @@ const app = express();
 //*INITIALIZE AND CONNECT TO DB
 connectDB();
 
-
 //*MIDDLEWARE
 //  ! replaced body parser
-app.use(express.json({extended:false}))
-
+app.use(express.json({ extended: false }));
 
 // app.use(responseTime);
 //const responseTime = require('./middleware/responseTime');
-
-
-
-//*ROUTE TEST CHECK
-app.get('/', (req, res) =>
-  res.json({ msg: 'Welcome to the Contact Book Manager API' })
-);
 
 //*DEFINED ROUTES, CALLS ROUTE FOLDER
 app.use('/api/users', require('./routes/users'));
@@ -33,5 +25,13 @@ app.use('/api/auth', require('./routes/auth'));
 app.use('/api/contacts', require('./routes/contacts'));
 
 const PORT = process.env.PORT || 5000;
+
+//* Serve static assets in production, must be at this location of this file
+if (process.env.NODE_ENV === 'production') {
+  //*Set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (re,res) => res.sendFile(path.resolve(__dirname, 'client', 'build','index.html')));
+}
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
